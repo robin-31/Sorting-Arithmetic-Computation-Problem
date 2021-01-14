a7e6b037d97b9e01fd9c86dd7d5160da9af6b51a
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-14 11:51:46 +0530

    ADDED: read from dict and store in array

--- Computationproblem.sh
+++ Computationproblem.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 declare -A computation
-echo "Hello to all"
+declare -a arrcomputation
+#"Hello to all"
 read -p "enter your first value a ::" a
 read -p "enter your second value b ::" b
 read -p "enter your third value c ::" c
-
 result=$(( $a + $b * $c ))
 result2=$(( $a * $b + $c ))
 result3=$(( $c + $a / $b ))
@@ -14,3 +14,9 @@ computation[operation2]="$result2"
 computation[operation3]="$result3"
 computation[operation4]="$result4"
 echo ${computation[@]}
+counter=0
+for i in "${!computation[@]}"
+do
+	arrcomputation[$((counter++))]="${computation[$i]}"
+done
+echo "data stored into array ${arrcomputation[@]}"
