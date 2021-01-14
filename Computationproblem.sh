699edfbcaaafb518ee9a15d9e7ed97294d96aadd
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-14 11:45:02 +0530

    ADDED: all operation result stored in dict

--- Computationproblem.sh
+++ Computationproblem.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+declare -A computation
 echo "Hello to all"
 read -p "enter your first value a ::" a
 read -p "enter your second value b ::" b
@@ -8,4 +9,8 @@ result=$(( $a + $b * $c ))
 result2=$(( $a * $b + $c ))
 result3=$(( $c + $a / $b ))
 result4=$(( $a % $b + $c ))
-
+computation[operation]="$result"
+computation[operation2]="$result2"
+computation[operation3]="$result3"
+computation[operation4]="$result4"
+echo ${computation[@]}
